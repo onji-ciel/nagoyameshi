7fe6cefaf702da11fd6a6c1818ef633e749578d5
onji-ciel <onji.koichi@cielsystem.co.jp>
2025-03-15 14:55:38 +0900

    新規作成

---
+++ src/main/java/com/example/nagoyamashi/NagoyamashiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nagoyamashi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagoyamashiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagoyamashiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nagoyamashi/NagoyamashiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nagoyamashi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagoyamashiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
