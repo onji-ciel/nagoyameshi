d50757ece475a4d8ed7c4445c4939e111c5e7c1d
onji-ciel <onji.koichi@cielsystem.co.jp>
2025-03-26 21:29:37 +0900

    有料プラン登録機能追加

--- src/main/java/com/example/nagoyameshi/service/UserService.java
+++ src/main/java/com/example/nagoyameshi/service/UserService.java
@@ -2,7 +2,13 @@ package com.example.nagoyameshi.service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -57,6 +63,7 @@ public class UserService {
 		return userRepository.save(user);
 	}
 	
+	@Transactional
 	public void update(UserEditForm userEditForm) {
 		User user = userRepository.getReferenceById(userEditForm.getId());
 		
@@ -81,6 +88,33 @@ public class UserService {
 		user.setEmail(userEditForm.getEmail());
 		
 		userRepository.save(user);
+	}
+	
+	@Transactional
+	public void saveStripeCustomerId(User user, String stripeCustomerId) {
+		user.setStripeCustomerId(stripeCustomerId);
+		userRepository.save(user);
+	}
+	
+	@Transactional
+	public void updateRole(User user, String roleName) {
+		Role role = roleRepository.findByName(roleName);
+		user.setRole(role);
+		userRepository.save(user);
+	}
+	
+	// 認証情報のロールを更新する
+	public void refreshAuthenticationByRole(String newRole) {
+		// 現在の認証情報を取得する
+		Authentication currentAuthentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		// 新しい認証情報を作成する
+		List<SimpleGrantedAuthority> simpleGrantedAuthorities = new ArrayList<>();
+		simpleGrantedAuthorities.add(new SimpleGrantedAuthority(newRole));
+		Authentication newAuthentication = new UsernamePasswordAuthenticationToken(currentAuthentication.getPrincipal(), currentAuthentication.getCredentials(), simpleGrantedAuthorities);
+		
+		// 認証情報を更新する
+		SecurityContextHolder.getContext().setAuthentication(newAuthentication);
 	}
 	
 	// メールアドレスが登録済みかチェック
